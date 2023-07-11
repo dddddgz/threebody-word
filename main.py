b9768fcac0efbeeed76dcb5360723d42a11d916d
dddddgz <1978563501@qq.com>
2023-07-11 20:42:25 +0800

    更新主程序、三体文本、README

---
+++ main.py
@@ -0,0 +1,19 @@
+import jieba
+import wordcloud as cloud
+from rich.console import Console
+from collections import Counter
+jieba.setLogLevel(jieba.logging.INFO)
+
+console = Console()
+console.log("正在读取三体文件……")
+with open("Threebody.txt", encoding="utf-8") as f:
+    content = f.read()
+console.log("三体文件读取完毕，正在分词……")
+words = jieba.lcut(content)
+console.log("分词完毕，正在过滤无效词语……")
+words = [word for word in words if len(word) > 1]
+console.log("无效词语过滤完毕，正在绘制词云……")
+wc = cloud.WordCloud(font_path="C:/Windows/Fonts/msyh.ttc",
+              background_color="white")
+wc.fit_words(dict(Counter(words)))
+wc.to_file("result.png")
